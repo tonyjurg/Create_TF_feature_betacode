a2bb2de208d442dd9d7fca15e6584285b8b86311
tonyjurg <github@jurgnet.nl>
2025-05-26 23:30:13 +0200

    update notebook

--- tf/1.0.0/betacode.tf
+++ tf/1.0.0/betacode.tf
@@ -3,15 +3,15 @@
 @convertedBy=Tony Jurg
 @coreData=Nestle 1904 Text-Fabric (centerBLC)
 @coreDataUrl=https://github.com/CenterBLC/N1904
-@description=Betacode representation of the surface word
-@license=MIT License
-@licenseUrl=https://mit-license.org/
+@description=Betacode representation of the surface level word (N1904-TF feature: text)
+@license=Creative Commons Attribution 4.0 International (CC BY 4.0)
+@licenseUrl=https://github.com/tonyjurg/N1904addons/blob/main/LICENSE.md
 @provenance=jupyter Notebook (https://github.com/tonyjurg/create_TF_feature_betacode)
 @valueType=str
 @version=1.0.0
 @website=https://github.com/tonyjurg/N1904addon
 @writtenBy=Text-Fabric
-@dateWritten=2025-05-02T10:36:22Z
+@dateWritten=2025-05-26T21:23:35+00:00Z
 
 *bi/blos
 gene/sews
